b8551fdc212d5b96d03703f32e41e4bb79fe7515
Flow-matic <jonathonvinson22@gmail.com>
2022-10-05 12:00:44 +0000

    Added urls path

--- blockchain/settings.py
+++ blockchain/settings.py
@@ -46,11 +46,11 @@ INSTALLED_APPS = [
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
+    'crispy_forms',
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
     'django_summernote',
-    'crispy_forms',
     'blog',
 ]
 
@@ -97,12 +97,12 @@ WSGI_APPLICATION = 'blockchain.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-#DATABASES = {
+# DATABASES = {
 #    'default': {
 #        'ENGINE': 'django.db.backends.sqlite3',
 #        'NAME': BASE_DIR / 'db.sqlite3',
 #    }
-#}
+# }
 
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
