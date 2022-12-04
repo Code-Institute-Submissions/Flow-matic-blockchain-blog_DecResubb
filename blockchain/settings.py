4b40a02aaf380628c02c36a99863289390167276
Flow-matic <jonathonvinson22@gmail.com>
2022-12-04 15:16:27 +0000

    Removed temporary secret key ready for re-submission

--- blockchain/settings.py
+++ blockchain/settings.py
@@ -26,7 +26,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', 'viakp!icby(*28g%)9qq59gggd1n79g7m1ki5wxfpe6%hc(7*l')
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
