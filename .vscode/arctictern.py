3d1a8f1816ebfd9f8ab362d7c3e3c355e6c7b63b
Flow-matic <jonathonvinson22@gmail.com>
2022-12-04 17:22:00 +0000

    Cleaning up any errors within the .py files using the python3 -m flake8 command

--- .vscode/arctictern.py
+++ .vscode/arctictern.py
@@ -89,7 +89,7 @@ def needs_upgrade():
         THIS_VERSION = 1.0
         with open(".vscode/version.txt", "w") as f:
             f.write(str(THIS_VERSION))
-    
+                
     r = requests.get(BASE_URL + ".vscode/version.txt")
     CURRENT_VERSION = float(r.content)
     print(f"Upstream version: {CURRENT_VERSION}")
--- blog/urls.py
+++ blog/urls.py
@@ -7,7 +7,14 @@ urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
     path('like/<slug:slug>', views.PostLike.as_view(), name='post_like'),
-    path('post/<int:pk>', views.PostDeleteView.as_view(), name='comment_delete'),
-    path('post/update<int:pk>', views.CommentUpdateView.as_view(), name='comment_update'),
-    
+    path(
+        'post/<int:pk>',
+        views.PostDeleteView.as_view(),
+        name='comment_delete'
+        ),
+    path(
+        'post/update<int:pk>',
+        views.CommentUpdateView.as_view(),
+        name='comment_update'
+        ),
 ]
